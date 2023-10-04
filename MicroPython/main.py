bacf86d5888bb7558eee6df63cf9c16b216b66da
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-04 16:55:20 +0000

    PYVER

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,21 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Ernest
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
+display.clear
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+# variable for cookies clicked
+cookies_clicked = 0
+
+while True:
+    if button_a.is_pressed():
+        cookies_clicked = cookies_clicked + 1
+        display.show(cookies_clicked)
+    if button_b.is_pressed():
+        cookies_clicked = 0
+        display.show(cookies_clicked)
